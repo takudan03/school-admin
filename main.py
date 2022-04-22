0f8e16c5040f9fbbaac7c3f92261d303b309730e
d-hwezha <Takudan@hotmail.com>
2022-04-22 16:38:51 +0200

    Update project-almost final product

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 import pymongo
-from flask_login import login_required, current_user, UserMixin, LoginManager, login_user,logout_user
+from flask_login import login_required, current_user, UserMixin, LoginManager, login_user, logout_user
 from classes.AnnouncementClass import Announcement
 from datetime import timedelta
 from datetime import datetime
@@ -11,41 +11,43 @@ import time
 
 # declare app variable
 app = Flask("Admin")
+app.secret_key = '12345'
 
-app.secret_key='12345'
-
-title="Jupyter Dashboard"
+title = "Jupyter Dashboard"
 heading = "TODO Reminder with Flask and MongoDB"
 
-client=pymongo.MongoClient("mongodb://127.0.0.1:27017")
-db=client.school_admin
-todos=db.todo
-username = "takudan123"
-role="Admin"
-student_id=0
+client = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+db = client.school_admin
 
-
-#login code
-login_manager=LoginManager(app)
-dbuser= db.users.find_one()
-users=db.users.find()
+# login code
+login_manager = LoginManager(app)
+dbuser = db.users.find_one()
+users = db.users.find()
 
 
 class User(UserMixin):
-    def __init__(self, id, email, pw):
-        self.id=id
-        self.email=email
-        self.password=pw
-        if self.id[0]=='S': self.role="Student"
-        elif self.id[0]=='T': self.role= "Teacher"
-        elif self.id[0]=='A': self.role="Administrator"
+    def __init__(self, id, pw):
+        self.id = id
+        self.password = pw
+        if self.id[0] == 'S':
+            self.role = "Student"
+            res=db.students.find_one({'_id': id})
+            self.name=res['firstname'] + ' ' + res['lastname']
+        elif self.id[0] == 'T':
+            self.role = "Teacher"
+            res=db.teachers.find_one({'_id': id})
+            self.name=res['firstname'] + ' ' + res['lastname']
+        elif self.id[0] == 'A':
+            self.role = "Administrator"
+            res=db.admins.find_one({'_id': id})
+            self.name=res['firstname'] + ' ' + res['lastname']
+
 
     @staticmethod
     def get(user_id: str) -> Optional["User"]:
-        result=db.users.find_one({'_id':user_id})
+        result = db.creds.find_one({'_id': user_id.upper()})
         if result:
-            userfound=User(result['_id'],result['email'],result['pw'])
-            return userfound
+            return User(result['_id'], result['pw'])
 
     def __str__(self) -> str:
         return f"<Id: {self.id}, Role: {self.role}>"
@@ -54,43 +56,48 @@ class User(UserMixin):
         return self.__str__()
 
 
-existing_user=User(id=dbuser['_id'],
-               email=dbuser['email'],
-               pw=dbuser['pw'])
-
-
-print(existing_user)
-
 @login_manager.user_loader
-def load_user(user_id)->Optional[User]:
+def load_user(user_id) -> Optional[User]:
     return User.get(user_id)
+
+@app.context_processor
+def inject_global_vars():
+    return {'users_logged_in': int(db.db_indices.find_one({'_id': 'users_logged_in'})['counter'])}
 
 
 def redirect_url():
     return request.args.get('next') or request.referrer or url_for('index')
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
     if request.method == 'POST':
+        user_id = request.form["id"]
+        user = User.get(user_id)
+        if user and request.form["psw"] != user.password:
+            db.creds.update_one({'_id':user.id},
+                                {'$inc': {'attempts': 1}},
+                                upsert=True
+                                )
+            flash("Incorrect Login Details. Please try again!")
 
-        id=request.form["uname"]
-
-        #queery users doc for user with id
-        #user=User.get(id)
-
-        pw=request.form["psw"]
-        if id==existing_user.id and pw==existing_user.password:
-            login_user(existing_user)
-
-            flash("User Logged in Succesfully!")
-            time.sleep(1)
-            print(current_user)
+        elif user and request.form["psw"] == user.password:
+            login_user(user)
+            print("Successful Login: " + str(current_user))
+            db.creds.update_one({'_id':user_id},
+                                {'$set': {'attempts': 0}},
+                                upsert=True
+                                )
+            db.db_indices.update_one({'_id': 'users_logged_in'},
+                                     {'$inc': {'counter': 1}},
+                                     upsert=True
+                                     )
             return redirect(url_for('dashboard'))
         else:
             flash("Incorrect Login Details. Please try again!")
+
 
     return render_template('login.html')
 
@@ -108,7 +115,13 @@ def dashboard():
 @app.route("/logout")
 @login_required
 def logout():
+    print("User logout: " + str(current_user))
     logout_user()
+    res = db.db_indices.update_one({'_id': 'users_logged_in'},
+                                   {'$inc': {'counter': -1}},
+                                   upsert=True
+                                   )
+    print(res)
     return redirect(url_for('login'))
 
 
@@ -116,23 +129,37 @@ def logout():
 def simple_render():
     return "This is to show how Flask renders web pages"
 
+
 @login_required
-@app.route("/subjects")
-def subjects(category=None):
-    # cursor=db.students.find_one({'_id':existing_user.id})
-    print(request.args.get("category"))
-    print(category)
+@app.route("/my-subjects")
+def my_subjects(category=None):
+    if current_user.role=="Administrator" or current_user.role=="Teacher":
+        return redirect(url_for("all_subjects"))
+    else:
+        # cursor=db.students.find_one({'_id':existing_user.id})
+        if request.args.get("category") is None:
+            print(request.args.get("category"))
+            category = "current"
+        else:
+            category = request.args.get("category")
+        cursor = db.students.find_one({'_id': current_user.id})
+        subjects_codes = list(cursor['subjects'][category])
+        subjects=[]
+        for i in subjects_codes:
+            subjects.append(db.courses.find_one({'_id':i}))
 
-    if request.args.get("category") is None: category="current"
-    else: category=request.args.get("category")
-    cursor=db.students.find_one({'_id':'S00003'})
-    subjects=cursor['subjects'][category]
+        print(subjects)
 
-    print(subjects)
+        return render_template('my_subjects.html', subjects=subjects, cat=category, user=current_user)
 
 
-    return render_template('subjects.html', subjects=subjects, cat=category,user=current_user)
+@login_required
+@app.route("/all-subjects")
+def all_subjects():
 
+        subjects = list(db.courses.find())
+        print(subjects)
+        return render_template('all_subjects.html', subjects=subjects, user=current_user)
 
 @login_required
 @app.route("/addstudent", methods=['GET', 'POST'])
@@ -142,7 +169,7 @@ def addstudent():
     if request.method == 'POST':
         # if request.form['submit'].get == 'Do Something':
         #     pass # do something
-        stu=request.form.get("name")
+        stu = request.form.get("name")
         print(stu)
         all_students.append(stu)
         flash("Student Added!", "success")
@@ -151,34 +178,106 @@ def addstudent():
     return render_template('add_student.html', t=title, user=current_user)
 
 @login_required
+@app.route("/addteacher", methods=['GET', 'POST'])
+def addteacher():
+    all_students = db.student_names.find()
+
+    if request.method == 'POST':
+        # if request.form['submit'].get == 'Do Something':
+        #     pass # do something
+        stu = request.form.get("name")
+        print(stu)
+        all_students.append(stu)
+        flash("Student Added!", "success")
+        return redirect('/')
+
+    return render_template('add_teacher.html', t=title, user=current_user)
+
+
+@login_required
+@app.route("/add-announcement", methods=['GET', 'POST'])
+def add_announcement():
+
+    if request.method == 'POST':
+        # if request.form['submit'].get == 'Do Something':
+        #     pass # do something
+        id=db.db_indices.find_one({'_id': "index_ann"})['curr_index']
+        db.db_indices.update_one({'_id': 'index_ann'},
+                                 {'$inc': {'curr_index': 1}},
+                                 upsert=True
+                                 )
+        title=request.form['title']
+        body=request.form['body']
+        recepient=request.form['announcements_recepients']
+        new_announcement={
+            '_id':'ANN' + str(id).rjust(5, '0'),
+            'date': datetime.now().strftime('%Y%m%d%H%M'),
+            'title': title,
+            'body': body,
+            'sender': current_user.name,
+            'recepient': recepient
+        }
+        print(new_announcement)
+        res=db.announcements.insert_one(new_announcement)
+        print(res.inserted_id)
+        flash("Announcement Sent to "+recepient+'!', "success")
+        return redirect(url_for('view_announcements'))
+
+    return render_template('add_announcement.html', t="New Announcement", user=current_user)
+
+
+
+@login_required
+@app.route("/addsubject", methods=['GET', 'POST'])
+def addsubject():
+    all_students = db.student_names.find()
+
+    if request.method == 'POST':
+        # if request.form['submit'].get == 'Do Something':
+        #     pass # do something
+        stu = request.form.get("name")
+        print(stu)
+        all_students.append(stu)
+        flash("Student Added!", "success")
+        return redirect('/')
+
+    return render_template('add_teacher.html', t=title, user=current_user)
+
+
+@login_required
 @app.route("/students")
 def students():
-    all_students = db.student_names.find()
-    student_count=len(list(all_students))
-    return render_template('students.html', students=all_students, student_count=student_count, t=title, user=current_user)
+    all_students = list(db.students.find())
+    student_count = len(list(all_students))
+    return render_template('students.html', students=all_students, student_count=student_count, t=title,
+                           user=current_user)
+
 
 @login_required
 @app.route("/teachers")
 def teachers():
-    # TODO Students and Teachers cannot edit teacher info. Only view it. Admins can edit teacher info.
-    teachers = db.student_names.find()
-    return render_template('teachers.html', students=teachers, t=title, user=current_user)
+    all_teachers = db.teachers.find()
+    return render_template('teachers.html', teachers=all_teachers, t="Teachers", user=current_user)
+
 
 @login_required
 @app.route("/announcements")
 def view_announcements():
-    announcements = db.announcements.find()
+
+    announcements = db.announcements.find({"$or": [{"recepient": 'All'}, {"recepient": current_user.role}]}).sort("date", -1)
 
     return render_template('announcements.html', announcements=announcements, t=title, user=current_user)
+
 
 @login_required
 @app.route("/student-info/<student_id>")
 def view_student(student_id):
-    student=db.student_names.find_one({'_id': student_id})
+    student = db.students.find_one({'_id': student_id})
     if student != None:
         return render_template('student_details.html', student=student, t=title, user=current_user)
     else:
-        return url_for('page_not_found', category='Student', t=title, user=current_user)
+        return url_for('page_not_found', category='Student', t="Student", user=current_user)
+
 
 # TODO: change route to announcements/view_announcements/<id>
 @login_required
@@ -187,47 +286,102 @@ def view_announcement(announcement_id):
     ann = db.announcements.find_one({'_id': announcement_id})
 
     if ann != None:
-        return render_template('view_announcement.html', ann=ann, t=ann['title']+" - From: " + ann['sender'])
+        return render_template('view_announcement.html', ann=ann, user=current_user, t=ann['title'] + " - From: " + ann['sender'])
     else:
-        return render_template('404.html', t=title, role=role, user=current_user)
+        return render_template('404.html', t=title, user=current_user)
+
 
 @login_required
-@app.route("/subjects/view-subject/<subject_id>")
+@app.route("/subjects/view-subject/<subject_id>", methods=['POST', "GET"])
 def view_subject(subject_id):
     subject = db.courses.find_one({'_id': subject_id})
+    students=db.students.find()
+    print(subject)
+    if subject is None:
+        return render_template('404.html', t=title, user=current_user, cat="Subject")
+    elif request.method == 'POST':
+        if request.form['submit'] == "Drop Subject":
+            db.students.update_one({'_id':current_user.id},
+                               {'$pull':{
+                                   'subjects.current':subject_id
+                               }
+                               }
+                               )
+            db.courses.update_one({'_id':subject_id},
+                                {'$pull':{'enrolled_students':current_user.id}}
+                                )
+            db.courses.update_one({'_id': subject_id},
+                                {'$inc': {'current_headcount': -1}}
+                                )
 
-    if subject != None:
-        return render_template('view_subject.html', subject=subject, t='View Subject', user=current_user)
+            print("Dropped subject in DB")
+        elif request.form['submit'] == "Register for Subject":
+            db.students.update_one({'_id':current_user.id},
+                               {'$push':{'subjects.current': subject_id}}
+                               )
+            db.courses.update_one({'_id':subject_id},
+                                {'$push':{'enrolled_students':current_user.id}}
+                                )
+            db.courses.update_one({'_id': subject_id},
+                                {'$inc': {'current_headcount': 1}}
+                                )
+            print("Registered for subject in DB")
+
+        return redirect(url_for('view_subject', subject_id=subject['_id']))
+
     else:
-        return render_template('404.html', t=title,user=current_user)
+        subject = db.courses.find_one({'_id': subject_id})
+        print(subject)
+        return render_template('view_subject.html', subject=subject,students=students, t='View Subject', user=current_user)
 
+
+@login_required
 @app.errorhandler(404)
 def page_not_found(e, category="Page"):
     # note that we set the 404 status explicitly
     return render_template('404.html', cat=category.upper(), user=current_user), 404
 
+
 @app.route("/layout")
 def layout():
-    title="Example of a Template"
+    title = "Example of a Template"
     return render_template('layout.html', t=title)
+
 
 @login_required
 @app.route("/profile")
 def profile():
-    student=db.students.find_one({'_id':current_user.id})
-    print(student)
-    return render_template('student_details.html', student=student, t="Profile", user=current_user)
+    uid= current_user.id
+    if uid[0] == "S":
+        this_user=db.students.find_one({'_id': current_user.id})
+    elif uid[0] == "T":
+        this_user=db.teachers.find_one({'_id': current_user.id})
+    elif uid[0] == "A":
+        this_user=db.admins.find_one({'_id': current_user.id})
+
+    print(this_user)
+    return render_template('profile.html', this_user=this_user, t="Profile", user=current_user)
 
 
 @app.template_filter()
 def format_datetime(value, format='date'):
     if format == 'date':
-        formatted_date=datetime.strptime(value, "%d%m%Y").strftime("%d/%m/%Y")
+        formatted_date = datetime.strptime(value, "%d%m%Y").strftime("%d/%m/%Y")
     elif format == 'datetime':
-        formatted_date=datetime.strptime(value, "%Y%m%d%H%M").strftime("%d/%m/%Y %I:%M %p")
+        formatted_date = datetime.strptime(value, "%Y%m%d%H%M").strftime("%d/%m/%Y %I:%M %p")
     return formatted_date
 
 
+@app.template_filter()
+def count_results(collection):
+    return len(list(collection))
+
+@app.template_filter()
+def get_student_name(id):
+    student=db.students.find_one({'_id': id})
+    if student:
+        return student['firstname'] + " " + student['lastname']
+    else:
+        return None
 if __name__ == "__main__":
     app.run()
-
